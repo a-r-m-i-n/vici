67794e59c8f91a51e60e3eed7078f0158ab8229f
Armin Vieweg <armin@v.ieweg.de>
2025-05-13 22:00:05 +0200

    [TASK] Add extension icon

---
+++ Configuration/Icons.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'vici-extension-icon' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:vici/Resources/Public/Icons/Extension.svg',
+    ],
+];
