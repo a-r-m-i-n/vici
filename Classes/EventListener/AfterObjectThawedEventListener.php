7b0044b19432ac47250bc9d65df00fc06d092da1
Armin Vieweg <armin@v.ieweg.de>
2025-05-23 10:10:07 +0200

    [TASK] Add __toString method to GenericViciModel

--- Classes/EventListener/AfterObjectThawedEventListener.php
+++ Classes/EventListener/AfterObjectThawedEventListener.php
@@ -2,20 +2,33 @@
 
 namespace T3\Vici\EventListener;
 
+use T3\Vici\Generator\StaticValues;
 use T3\Vici\Model\GenericViciModel;
 use TYPO3\CMS\Core\Attribute\AsEventListener;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Event\Persistence\AfterObjectThawedEvent;
 
 #[AsEventListener(
     identifier: 'vici/after-object-thawed',
 )]
-class AfterObjectThawedEventListener
+readonly class AfterObjectThawedEventListener
 {
+    public function __construct(private StaticValues $staticValues)
+    {
+    }
+
     public function __invoke(AfterObjectThawedEvent $event): void
     {
         $object = $event->getObject();
         if ($object instanceof GenericViciModel) {
             $object->_record = $event->getRecord();
+
+            // Set _tablename
+            $classNameParts = GeneralUtility::trimExplode('\\', get_class($object), true);
+            $name = array_pop($classNameParts);
+            if ($name) {
+                $object->_tablename = $this->staticValues->getFullTableName(GeneralUtility::camelCaseToLowerCaseUnderscored($name));
+            }
         }
     }
 }
--- Classes/Model/GenericViciModel.php
+++ Classes/Model/GenericViciModel.php
@@ -10,4 +10,21 @@ abstract class GenericViciModel extends AbstractEntity
      * @var array<string, mixed>
      */
     public array $_record = [];
+
+    public string $_tablename = '';
+
+    public function __toString(): string
+    {
+        $labelField = $GLOBALS['TCA'][$this->_tablename]['ctrl']['label'] ?? null;
+        $label = '';
+        if ($labelField && array_key_exists($labelField, $this->_record)) {
+            $label = $this->_record[$labelField] ?? '';
+        }
+
+        if (!empty($label)) {
+            return $label;
+        }
+
+        return $this->_tablename . ':' . $this->_record['uid'];
+    }
 }
