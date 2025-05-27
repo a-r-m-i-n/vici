846d867ebe0fd5a8967d1f96497b3be8530340bc
Armin Vieweg <armin@v.ieweg.de>
2025-05-27 11:03:55 +0200

    [BUGFIX] Change permission logic in AvailableViciTables ItemsProcFunc
    Non-admin users may choose vici table in frontend plugin, if the user has write access to the table.

--- Classes/UserFunction/ItemsProcFunc/AvailableViciTables.php
+++ Classes/UserFunction/ItemsProcFunc/AvailableViciTables.php
@@ -2,10 +2,9 @@
 
 namespace T3\Vici\UserFunction\ItemsProcFunc;
 
+use T3\Vici\Generator\StaticValues;
 use T3\Vici\Repository\ViciRepository;
-use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
-use TYPO3\CMS\Core\Type\Bitmask\Permission;
 
 /**
  * Shows only available vici tables, respecting the access permissions of current user.
@@ -16,6 +15,7 @@ readonly class AvailableViciTables
 
     public function __construct(
         private ViciRepository $viciRepository,
+        private StaticValues $staticValues,
     ) {
         $this->backendUser = $GLOBALS['BE_USER'];
     }
@@ -31,17 +31,16 @@ readonly class AvailableViciTables
                 continue;
             }
 
-            $pageRow = BackendUtility::getRecord('pages', $tableRow['pid']) ?? [];
-            $pageShow = $this->backendUser->doesUserHaveAccess($pageRow, Permission::PAGE_SHOW);
-            $pageEditContents = $this->backendUser->doesUserHaveAccess($pageRow, Permission::CONTENT_EDIT);
-
-            if ($pageShow && $pageEditContents) {
-                $parameters['items'][] = [
-                    'label' => !empty($tableRow['title']) ? $tableRow['title'] : ucfirst($tableRow['name']),
-                    'value' => $tableRow['uid'],
-                    'icon' => $tableRow['icon'],
-                ];
+            $tableName = $this->staticValues->getFullTableName($tableRow['name']);
+            if (!$this->backendUser->isAdmin() && !$this->backendUser->check('tables_modify', $tableName)) {
+                continue;
             }
+
+            $parameters['items'][] = [
+                'label' => !empty($tableRow['title']) ? $tableRow['title'] : ucfirst($tableRow['name']),
+                'value' => $tableRow['uid'],
+                'icon' => $tableRow['icon'],
+            ];
         }
 
         if (!empty($parameters['items'])) {
